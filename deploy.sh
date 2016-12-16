173daaf5144b52d1933e995e3d1438f9039af0e1
Skyfold <pfalzgraf@drmartin.com>
2016-12-16 13:17:47 +0800

    setup circleCI, puts artifact on github releases

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cat <<EOT >> .netrc
+machine api.github.com
+  login Skyfold
+  password $GITHUB_TOKEN
+machine upload.github.com
+  login skyfold
+  password $GITHUB_TOKEN
+EOT
+
+./github_deploy.sh SkyFold/wstunnel v0.1.0.0 -- dist/build/wstunnel/wstunnel
