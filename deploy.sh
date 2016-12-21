8969cd936317b3d9818eacf16c93a08552a7f390
Skyfold <pfalzgraf@drmartin.com>
2016-12-21 11:45:53 +0800

    forgot the bin prefix

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ machine upload.github.com
   password $GITHUB_TOKEN
 EOT
 
-tar czf wstunnel.tar.gz wstunnel
-tar czf wstunnel_windows_x64.tar.gz wstunnel_windows_x64/
-tar czf wstunnel_windows_x86.tar.gz wstunnel_windows_x86/
-tar czf wstunnel_x86_64.tar.gz wstunnel_x86_64
+tar czf bin/wstunnel.tar.gz bin/wstunnel
+tar czf bin/wstunnel_windows_x64.tar.gz bin/wstunnel_windows_x64/
+tar czf bin/wstunnel_windows_x86.tar.gz bin/wstunnel_windows_x86/
+tar czf bin/wstunnel_x86_64.tar.gz bin/wstunnel_x86_64
 
 ./github_deploy.sh SkyFold/wstunnel v0.1.0.0 -- bin/*.tar.gz
